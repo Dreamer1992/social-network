8b5278150b4a6712c7bc66d19ac7157059d5dc5a
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2020-10-11 22:26:43 +0300

    Chat 42 redux

---
+++ src/redux/reduxStore.js
@@ -0,0 +1,14 @@
+import {combineReducers, createStore} from "redux";
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+import sidebarReducer from "./sidebarReducer";
+
+let reducers = combineReducers({
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  sidebar: sidebarReducer
+});
+
+let store = createStore(reducers);
+
+export default store;
