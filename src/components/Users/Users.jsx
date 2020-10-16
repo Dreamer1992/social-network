0da6f3060293605c68eeba43515f3153208124b6
Давлет Ганиев <ganiev.davlet@gmail.com>
2020-10-16 17:59:47 +0300

    62 FOLLOW/UNFOLLOW API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Users.module.css";
 import userPhoto from "../../assets/images/default_avatar.png";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -31,10 +32,30 @@ let Users = (props) => {
           </div>
           {user.followed
             ? <button onClick={() => {
-              props.unfollow(user.id)
+              axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {}, {
+                withCredential: true,
+                headers: {
+                  'API-KEY': '49868490-96c7-4528-94fc-999e9e5ae9c5'
+                }
+              })
+                .then(response => {
+                  if (response.data.resultCode === 0) {
+                    props.follow(user.id);
+                  }
+                });
             }}>Follow</button>
             : <button onClick={() => {
-              props.follow(user.id)
+              axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+                withCredentials: true,
+                headers: {
+                  'API-KEY': '49868490-96c7-4528-94fc-999e9e5ae9c5'
+                }
+              })
+                .then(response => {
+                  if(response.data.resultCode !== 0) {
+                    props.unfollow(user.id)
+                  }
+                })
             }}>Unfollow</button>
           }
         </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -15,21 +15,27 @@ import Preloader from "../common/Preloader/Preloader";
 class UsersComponent extends React.Component {
   componentDidMount() {
     this.props.toggleIsFetching(true);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count${this.props.pageSize}`).then(response => {
-      this.props.toggleIsFetching(false);
-      this.props.setUsers(response.data.items);
-      this.props.setTotalUsersCount(response.data.totalCount);
-    });
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count${this.props.pageSize}`, {
+      withCredentials: true
+    })
+      .then(response => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
   }
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
 
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count${this.props.pageSize}`).then(response => {
-      this.props.toggleIsFetching(false);
-      this.props.setUsers(response.data.items)
-    });
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count${this.props.pageSize}`, {
+      withCredentials: true
+    })
+      .then(response => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items)
+      });
   }
 
   render() {
