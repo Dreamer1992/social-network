eed74414695a829ae8bb0b220187aa5355b057ca
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2020-10-25 20:21:46 +0300

    Updates

---
+++ precache-manifest.8c964e8d769e9275bdb041f291055f8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e4a0d2f3fc855fc9b5474e714dba48",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "e71a0fd4dd8c1afcdf74",
+    "url": "/social-network/static/css/main.bedfc1bc.chunk.css"
+  },
+  {
+    "revision": "9b847ba0a618926360ab",
+    "url": "/social-network/static/js/2.bcef308c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.bcef308c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a0fd4dd8c1afcdf74",
+    "url": "/social-network/static/js/main.9f25bbf3.chunk.js"
+  },
+  {
+    "revision": "524845359e191605bf24",
+    "url": "/social-network/static/js/runtime-main.6854f22a.js"
+  },
+  {
+    "revision": "414267b29d268138ad0674e20cb7fbac",
+    "url": "/social-network/static/media/default_avatar.414267b2.png"
+  },
+  {
+    "revision": "fd3051a6ce494c19c9fa5468e5d43a6e",
+    "url": "/social-network/static/media/preloader.fd3051a6.gif"
+  }
+]);
